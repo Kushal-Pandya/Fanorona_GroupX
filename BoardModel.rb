d0dc1db02eab223eb0a550bc2eed053ca82f5fa4
Kushal Pandya <kushalpandya@Kushals-MacBook-Pro.local>
2017-11-05 21:51:18 -0500

    converting tabs to spaces

--- BoardModel.rb
+++ BoardModel.rb
@@ -1,23 +1,23 @@
 class BoardModel
-	def initialize(rows, columns)
-		@stones = nil
-	  	@rows= rows
-	  	@columns = columns
-	  	rules = nil
-	  	subject = nil
-		select_stone = nil
-   	end
-   	def get_stone(row, column)
-   	end
-	def select_stone(row, column)
-   	end
-	def move_stone(row, column)
-   	end
-   	def capture_stone(row, column)
-   	end
+    def initialize(rows, columns)
+        @stones = nil
+        @rows= rows
+        @columns = columns
+        rules = nil
+        subject = nil
+        select_stone = nil
+    end
+    def get_stone(row, column)
+    end
+    def select_stone(row, column)
+    end
+    def move_stone(row, column)
+    end
+    def capture_stone(row, column)
+    end
 end
 
 class FanoronaBoardModel < BoardModel
-	def reset_stones()
-	end
+    def reset_stones()
+    end
 end
