ea8b0bb19afd456b3aaaccff6f3a39ab194f9cb9
Kushal Pandya <kushalpandya@Kushals-MacBook-Pro.local>
2017-11-09 15:59:37 -0500

    adding skeleton structure to observer child class

--- Observer.rb
+++ Observer.rb
@@ -7,4 +7,15 @@ class Observer
 end
 
 class StoneObserver < Observer
+	def initialize(observers)
+		@observers = nil
+	end
+	def register_observer(observer)
+	end
+	def unregister_observer(observer)
+	end
+	def get_state()
+	end
+	def set_state()
+	end
 end
--- RuleBook.rb
+++ RuleBook.rb
@@ -1,13 +1,19 @@
+require_relative "BoardView"
+require_relative "MoveOutcome"
+
 class RuleBook
-   def initialize()
-   end
-   def validate_selection(row, column)
-   end
-   def validate_move(row, column)
-   end
-   def validate_capture(row, column)
-   end
+    def initialize()
+        @boardView = BoardView.new()
+    end
+    def validate_selection(row, column)
+    end
+    def validate_move(row, column)
+    end
+    def validate_capture(row, column)
+    end
 end
+
+
 
 class FanoronaRuleBook < RuleBook
 end
