313629a82b9d96b5543d4e9f49413e10aad40f03
Unknown <jlemcke@mail.uoguelph.ca>
2017-11-10 13:14:08 -0500

    Auto stash before merge of "dev" and "origin/dev"

--- Game.rb
+++ Game.rb
@@ -1,9 +1,35 @@
 class Game
    def initialize()
-      @model = nil
-      @view = nil
-      @controller = nil
+      @view = BoardView.initialize()
+	  @model = BoardModel.initialize(5,9)
+	  
+	  @view.display_colour_selection()
+	  colourIn = @view.prompt_text()
+	  if colourIn == "1"
+	     p1Controller = playerController.initialize(@model, @view, Colour::White)
+		 p2Controller = playerController.initialize(@model, @view, Colour::Black)
+		 @controller = p1Controller
+      else
+	     p1Controller = playerController.initialize(@model, @view, Colour::Black)
+		 p2Controller = playerController.initialize(@model, @view, Colour::White)
+		 @controller = p2Controller
+	  end
+	  
    end
+   
+   
    def play()
+	  result = @controller.take_turn()
+	  while result == Outcome::Turn_over do
+	     if @controller == p1Controller
+		    @controller = p2Controller
+		 else
+		    @controller = p1Controller
+		 end
+		 result = @controller.take_turn()
+	  end
    end
+   
+   
+   
 end
