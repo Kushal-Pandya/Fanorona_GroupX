b5da398fc249c114751d4169a450acf7116e56c8
Unknown <jlemcke@mail.uoguelph.ca>
2017-11-11 15:30:02 -0500

    wrote main menu selections into play()

--- Subject.rb
+++ Subject.rb
@@ -1,15 +1,21 @@
 class Subject
-   def initialize()
-      @observers = nil
+   def initialize(view)
+      @observers = []
+	  @viewModel = view
       @state = nil
    end
    def register_observer(observer)
+      @observers << observer
    end
    def unregister_observer(observer)
+      @observers -= observer #no idea if this is correct
    end
    def get_state()
-       return @state
+       #return @state
+	   
+	   #
    end
    def set_state()
+      #what even actually goes here? there has to be an input for this to even make sense, right?
    end
 end
